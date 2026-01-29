58537ceb4fcdff9879e03fa2724013d54ca40e64
Roman <dev@rman.dev>
2026-01-29 22:17:22 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ if (!TOKEN) throw new Error("Missing TOKEN env variable");
 
 const bot = new Bot(TOKEN);
 
-bot.on("message").command("start", handleStart);
+bot.on("message").command(["start", "game", "draughts"], handleStart);
 bot.on("callback_query:data", (ctx) => {
   const data = ctx.callbackQuery.data;
   if (!data.startsWith("move:")) return;
